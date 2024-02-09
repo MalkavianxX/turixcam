80515f43ae456542e7fcefc50533fcb6d5bc83f1
MalkavianxX <mot46000@gmail.com>
2024-02-08 19:27:32 -0600

    ff

--- login/myadapter.py
+++ login/myadapter.py
@@ -2,4 +2,4 @@ from allauth.account.adapter import DefaultAccountAdapter
 
 class MyAccountAdapter(DefaultAccountAdapter):
     def get_login_redirect_url(self, request):
-        return 'http://127.0.0.1:8000/profile'
+        return 'https://turixcam.com/profile'
