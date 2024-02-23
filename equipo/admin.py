209b16ef8d65601980a9639fd879169b2c0022bf
MalkavianxX <mot46000@gmail.com>
2024-02-23 14:10:31 -0600

    modelsvisibilidad

--- equipo/admin.py
+++ equipo/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from .models import Personal  # Asegúrate de que la ruta de importación sea correcta
 
-class PersonalAdmin(admin.ModelAdmin):
-    exclude = ('descripcion', 'foto_perfil', 'foto_portada',)
 
-admin.site.register(Personal, PersonalAdmin)
+
+admin.site.register(Personal)
